92d9411a52fb2843e22ef8508aaf8b9a1b2f1885
Yeasin-Rifat <yeasinrifat1998@gmail.com>
2026-05-16 13:59:13 +0600

    modules and folder strucure done

--- my-portfolio-ui/src/app/app-routing.module.ts
+++ my-portfolio-ui/src/app/app-routing.module.ts
@@ -1,7 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './features/auth/login/login.component';
+
+import { HomeComponent } from './features/public/home/home.component';
+import { ProjectsComponent } from './features/public/projects/projects.component';
+import { SkillsComponent } from './features/public/skills/skills.component';
+import { ExperienceComponent } from './features/public/experience/experience.component';
+import { ContactComponent } from './features/public/contact/contact.component';
+
+import { DashboardComponent } from './features/admin/dashboard/dashboard.component';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent
+  },
+
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+
+  {
+    path: 'skills',
+    component: SkillsComponent
+  },
+
+  {
+    path: 'experience',
+    component: ExperienceComponent
+  },
+
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: 'admin/dashboard',
+    component: DashboardComponent
+  },
+
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-portfolio-ui/src/app/app.module.ts
+++ my-portfolio-ui/src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { HomeComponent } from './features/public/home/home.component';
+import { ProjectsComponent } from './features/public/projects/projects.component';
+import { ExperienceComponent } from './features/public/experience/experience.component';
+import { ContactComponent } from './features/public/contact/contact.component';
+import { DashboardComponent } from './features/admin/dashboard/dashboard.component';
+import { SkillsComponent } from './features/public/skills/skills.component';
+import { ProfileComponent } from './features/admin/profile/profile.component';
+import { ContactsComponent } from './features/admin/contacts/contacts.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    ContactComponent,
+    DashboardComponent,
+    SkillsComponent,
+    ProfileComponent,
+    ContactsComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
